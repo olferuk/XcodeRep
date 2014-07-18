f3c85c211343312d7821f3a0c0b54482a5eb886c
Alexander Olferuk <enlightened@Alexanders-Mac.local>
2014-07-18 09:16:51 -0700

    First commit lol

---
+++ Drawing/PlayingCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardViewController.h
+//  Drawing
+//
+//  Created by Alexander Olferuk on 18/07/14.
+//  Copyright (c) 2014 CSF_Enlightened. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayingCardViewController : UIViewController
+
+@end
---
+++ Drawing/PlayingCardViewController.m
@@ -0,0 +1,23 @@
+//
+//  PlayingCardViewController.m
+//  Drawing
+//
+//  Created by Alexander Olferuk on 18/07/14.
+//  Copyright (c) 2014 CSF_Enlightened. All rights reserved.
+//
+
+#import "PlayingCardViewController.h"
+
+@interface PlayingCardViewController ()
+
+@end
+
+@implementation PlayingCardViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
